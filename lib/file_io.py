d1de36cdd6b949f8f90d73f0ae93a57c80611adb
Mgridgelabs <mainaianndungu@gmail.com>
2024-09-05 23:18:25 +0300

    completes file IO lab

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,16 @@
+
 def write_file(file_name, file_content):
-    pass
+    full_file_name = file_name.with_suffix(".txt")
+    with open(full_file_name, mode='w', encoding='utf=8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+    full_file_name = file_name.with_suffix(".txt")
+    with open(full_file_name, mode='a', encoding='utf=8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    full_file_name = file_name.with_suffix(".txt")
+    with open(full_file_name, mode='r', encoding='utf=8') as file:
+        return file.read()
+    
